25768f61702378f6eb05cfbfc29e27481560ae6a
Lucas Nunes <lucassnunes2014@gmail.com>
2022-05-10 21:49:11 -0300

    Added Sum Mixed Array

---
+++ 8kyu Sum Mixed Array.js
@@ -0,0 +1,5 @@
+/*Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Return your answer as a number.*/
+
+
+const sumMix = (x) => x.map((a) => +a).reduce((acc, cur)=> acc + cur)
