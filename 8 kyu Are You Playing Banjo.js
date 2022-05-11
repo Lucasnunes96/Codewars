14c85850af4c3c1b0bbf06de540b5e2f4314c22d
Lucas Nunes <lucassnunes2014@gmail.com>
2022-05-11 11:26:55 -0300

    Updated

---
+++ 8 kyu Are You Playing Banjo.js
@@ -0,0 +1,16 @@
+/*Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.*/
+
+const areYouPlayingBanjo = (name) => 
+name.split('') // 'Ringo' => ['R','i','n','g','o']
+[0] // => gets 'R'
+.toLowerCase() // 'R' => 'r'
+ === 'r' ? 
+ `${name} plays banjo` // if equal 'r'
+ : `${name} does not play banjo` // if not equal 'r'
